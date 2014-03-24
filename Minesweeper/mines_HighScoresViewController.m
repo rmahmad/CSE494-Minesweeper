7d77049a7e6f4f4ddccbf1f3281ab9850f0077c0
Stephen Pluta <stephen.pluta@gmail.com>
2014-03-24 14:59:59 -0700

    commiting some awesome stuff

--- Minesweeper/mines_HighScoresViewController.m
+++ Minesweeper/mines_HighScoresViewController.m
@@ -10,6 +10,8 @@
 
 @interface mines_HighScoresViewController ()
 
+@property (strong, nonatomic) NSArray *logList;
+
 @end
 
 @implementation mines_HighScoresViewController
@@ -27,12 +29,67 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    self.logList = [[NSMutableArray alloc] init];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSString *)documentsDirectory
+{
+    return [@"~/Documents" stringByExpandingTildeInPath];
+}
+- (NSString *)dataFilePath
+{
+    return [[self documentsDirectory] stringByAppendingPathComponent:@"LoginModel.plist"];
+}
+
+- (void)loadChecklistItems
+{
+    // get our data file path
+    NSString *path = [self dataFilePath];
+    
+    //do we have anything in our documents directory?  If we have anything then load it up
+    if ([[NSFileManager defaultManager] fileExistsAtPath:path]) {
+        NSData *data = [[NSData alloc] initWithContentsOfFile:path];
+        // make an unarchiver, and point it to our data
+        NSKeyedUnarchiver *unarchiver = [[NSKeyedUnarchiver alloc] initForReadingWithData:data];
+        // We would like to unarchive the "ChecklistItems" key and get a reference to it
+        self.logList = [unarchiver decodeObjectForKey:@"HighScores"];
+        // we've finished choosing keys that we want, unpack them!
+        [unarchiver finishDecoding];
+    }
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.account.logList.count;
+}
+
+// Customize the appearance of table view cells.
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell;
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"BasicCell"];
+    }
+    
+    // Set the data for this cell to be from the logList
+    cell.textLabel.text = [[self.account.logList objectAtIndex:indexPath.row] objectAtIndex:0];
+    cell.detailTextLabel.text = [[self.account.logList objectAtIndex:indexPath.row] objectAtIndex:2];
+    cell.imageView.image = [UIImage imageNamed:[[self.account.logList objectAtIndex:indexPath.row] objectAtIndex:1]];
+    
+    return cell;
 }
 
 @end
